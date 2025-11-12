03eea44e5eeee002f88bfa3355d5a26f161cbac0
Tokar <tokarczykdawid075@gmail.com>
2025-11-12 22:08:04 +0100

    Modyfikacja Pliku .Dodanie zmiennej ZapiszTxt

--- Programowanie Zaawansowane 2 - Drzewo BST/PLIKI.cpp
+++ Programowanie Zaawansowane 2 - Drzewo BST/PLIKI.cpp
@@ -30,3 +30,24 @@ bool PLIKI::wczytajTxt(BST& tree, const std::string& filepath) {
 	return true;
 }
 
+bool PLIKI::zapiszTxt(const BST& tree, const std::string& filepath) {
+	std::ofstream out(filepath); // otwarcie lub zalozenie nowego pliku
+	if (!out) {
+		return false;
+	}
+
+	struct W {
+		static void run(BST::Node* node, std::ofstream& out) { //metoda rekurencyjne przechodzi po drzewie
+			if (!node) {
+				return;
+			}
+
+			out << node->value << ' '; // dzialanie jak preorder, liczby oddzielone spacjami
+			run(node->left, out); // dzialanie jak preorder
+			run(node->right, out); // dzialanie jak preorder
+		}
+	};
+	W::run(tree.root, out); // zaczynamy od roota drzewa
+	return true;
+}
+
